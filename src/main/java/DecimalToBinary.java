0e7755409a9a13eababd801e8f3f8466229fc5d0
cdf144 <neniha5230@ibtrades.com>
2023-10-24 09:45:36 +0700

    Added more algorithms

--- src/main/java/DecimalToBinary.java
+++ src/main/java/DecimalToBinary.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class DecimalToBinary {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DecimalToBinary {
+
+    public static String toBinaryString(long n) {
+        List<Integer> binaryArr = new ArrayList<>();
+        while (n > 0) {
+            if (n % 2 == 0) {
+                binaryArr.add(0);
+            } else {
+                binaryArr.add(1);
+            }
+            n /= 2;
+        }
+
+        StringBuilder binaryString = new StringBuilder(binaryArr.size());
+        for (int i = binaryArr.size() - 1; i >= 0; i--) {
+            binaryString.append(binaryArr.get(i));
+        }
+
+        return binaryString.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        System.out.print(toBinaryString(n));
+
+        bufferedReader.close();
+    }
 }
---
+++ src/main/java/LongFactorial.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Compute {
+
+    public static void extraLongFactorials(int n) {
+        int[] resultArr = new int[200];
+        resultArr[0] = 1;
+        int digitCount = 1;
+        int remain = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < digitCount; j++) {
+                int a = resultArr[j] * i + remain;
+                resultArr[j] = a % 10;
+                remain = a / 10;
+            }
+
+            while (remain > 0) {
+                resultArr[digitCount] = remain % 10;
+                remain /= 10;
+                digitCount++;
+            }
+        }
+
+        for (int i = digitCount - 1; i >= 0; i--) {
+            System.out.print(resultArr[i]);
+        }
+    }
+
+}
+
+public class LongFactorial {
+    public static void main(String[] args) throws IOException {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+      int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+      Compute.extraLongFactorials(n);
+
+      bufferedReader.close();
+    }
+}
